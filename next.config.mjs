b00fdfd77ba79385b185f2c204e7c249cb5e388a
Simran-1313 <singhsimranpreet916@gmail.com>
2024-04-16 05:44:07 +0530

    testing 2

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: '*.googleusercontent.com',
-          },
-          {
-            protocol:'https',
-            hostname:'**'
-          }
-        ],
-      },
+  images: { remotePatterns: [{ protocol: "https", hostname: "**" }] },
 };
 
 export default nextConfig;
