dddddba1372009fbaab0c2cabe0b0e2ea8fe2c10
Simran-1313 <singhsimranpreet916@gmail.com>
2024-04-16 06:28:40 +0530

    cheching the register page and login page

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link';
 import { signIn } from 'next-auth/react'
+import Navbar from '../components/Navbar';
 
 const Registerpage = () => {
   const [email,setEmail] = useState('');
@@ -42,7 +43,9 @@ const Registerpage = () => {
     
   }
   return (
-    <section className='mt-8' >
+   <>
+   <Navbar/>
+    <section className='mt-24 h-[70vh]' >
       <h1 className='text-center text-primary text-4xl mb-4 ' >
         Register
       </h1>
@@ -72,18 +75,11 @@ const Registerpage = () => {
           setPassword(e.target.value);
         }} />
         <button type='submit' disabled={creatingUser} > Register</button>
-        <div className='my-4 text-center text-gray-500' >
-        or login with provider
-        </div>
-        <button type='button' onClick={()=> signIn('google',{callbackUrl:'/'})} className='flex gap-4 justify-center ' >
-          <Image src='/google.png' alt='' width={24} height={24} /> Login with google</button>
-
-          <div className='text-center my-4 text-gray-500 border-t pt-4 ' >
-            Existing Account?&nbsp; <Link className='underline ' href={'/login'}> Login here&raquo;</Link>
-          </div>
+        
       </form>
       
     </section>
+   </>
   )
 }
 
--- src/app/steps/login/page.js
+++ src/app/steps/login/page.js
@@ -3,7 +3,7 @@ import React from 'react'
 import Image from 'next/image';
 import { useState } from 'react';
 import { signIn } from 'next-auth/react';
-import SmallNavbar from '@/app/components/SmallNavbar';
+import Navbar from '@/app/components/Navbar';
 import Link from 'next/link';
 const LoginPage = () => {
 
@@ -22,7 +22,7 @@ const LoginPage = () => {
 
   return (
     <>
-    <SmallNavbar/>
+    <Navbar/>
     <section className='mt-24 h-[70vh] ' >
         <h1 className='text-center text-primary text-4xl mb-4 ' >
         Login
